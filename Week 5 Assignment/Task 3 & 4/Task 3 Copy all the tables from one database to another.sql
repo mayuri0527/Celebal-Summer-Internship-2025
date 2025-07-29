506504166b30d3745fa7a422c6b181a58fed5901
MAYURI JAIN <159015391+mayuri0527@users.noreply.github.com>
2025-07-29 15:05:08 +0530

    Add files via upload

---
+++ Week 5 Assignment/Task 3 & 4/Task 3 Copy all the tables from one database to another.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE Employees;
+
+SELECT * INTO Employees.dbo.countries
+FROM HR.dbo.countries;
+
+SELECT * INTO Employees.dbo.departments
+FROM HR.dbo.departments;
+
+SELECT * INTO Employees.dbo.dependents
+FROM HR.dbo.dependents;
+
+SELECT * INTO Employees.dbo.employees
+FROM HR.dbo.employees;
+
+SELECT * INTO Employees.dbo.jobs
+FROM HR.dbo.jobs;
+
+SELECT * INTO Employees.dbo.locations
+FROM HR.dbo.locations;
+
+SELECT * INTO Employees.dbo.regions
+FROM HR.dbo.regions;
+
---
+++ Week 5 Assignment/Task 3 & 4/Task 4 Copy selective tables with selective columns from one database to another.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE selective_HR;
+
+SELECT country_id, country_name INTO selective_HR.dbo.new_countries
+FROM HR.dbo.countries;
+
+SELECT department_id, department_name INTO selective_HR.dbo.new_departments
+FROM HR.dbo.departments;
+
+SELECT dependent_id, first_name, last_name, relationship INTO selective_HR.dbo.new_dependents
+FROM HR.dbo.dependents;
+
+SELECT employee_id, first_name, last_name, salary INTO selective_HR.dbo.new_employees
+FROM HR.dbo.employees;
+
+SELECT job_id, job_title INTO selective_HR.dbo.new_jobs
+FROM HR.dbo.jobs;
+
+SELECT location_id, postal_code, city INTO selective_HR.dbo.new_locations
+FROM HR.dbo.locations;
+
+SELECT region_id, region_name INTO selective_HR.dbo.new_regions
+FROM HR.dbo.regions;
